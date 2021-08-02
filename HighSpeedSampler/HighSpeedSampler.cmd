74de1baa953230eba4f51c8086dc8421743f41fe
asytyi <alexander.a.sytyi@gmail.com>
2021-08-02 16:25:07 +0300

    Скрипт перезапуска переработан с паузой перед стартом и добавлен в проект

--- HighSpeedSampler/HighSpeedSampler.cmd
+++ HighSpeedSampler/HighSpeedSampler.cmd
@@ -3,13 +3,26 @@
 setlocal
 set APP=HighSpeedSampler.exe
 set APP_PATH=D:\HighSpeedSampler\
+set CNT=1
 
 pushd %APP_PATH%
+echo Pre-launch 30s pause
+ping 127.0.0.1 -n 30 > nul
 
 :loop
-
+echo %TIME%
+echo Attempt %CNT%
 tasklist /fi "ImageName eq %APP%" /fo csv 2>NUL | find /I "%APP%">NUL
 if "%ERRORLEVEL%"=="1" %APP_PATH%%APP%
-ping 127.0.0.1 -n 5 > nul
+ping 127.0.0.1 -n 10 > nul
+set /a CNT=%CNT%+1
+echo --------
 
-goto loop
+if "%CNT%"=="4" (
+	goto loop_exit
+) else (
+	goto loop
+)
+
+:loop_exit
+shutdown -r -f -t 0
